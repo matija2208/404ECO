4a622e0440c3fd57b860b472a372b48fbb94780b
Luka <lukamihgimnazija@gmail.com>
2022-03-12 23:30:42 +0100

    Objaa posta

--- FRONT/Javascript/unosenje.js
+++ FRONT/Javascript/unosenje.js
@@ -15,3 +15,104 @@ new Imgur({
     clientid: 'a08fd223eb9d597', //You can change this ClientID
     callback: feedback
 });
+
+////////////// vvv za upitnik i objavljivanje
+
+
+
+function generateInput(num){
+    var questionTemplate = `
+    <div id="Pitanje${num}">
+        <hr>
+        <h1 class="dropzone_narrate">Pitanje #${num}:</h1>
+
+        <textarea id="pitanje${num}_tekst_input" type='text' style="resize: vertical;" placeholder='Tekst pitanja:'></textarea>
+
+        <input class="odg_input" id="pitanje${num}_odg1_input" type='text' placeholder='Odgovor 1:'/> 
+        <input class="RadioButton" type="radio" name="pitanje${num}_radio" value="1">
+
+        <input class="odg_input" id="pitanje${num}_odg2_input" type='text' placeholder='Odgovor 2:'/> 
+        <input class="RadioButton" type="radio" name="pitanje${num}_radio" value="2">
+
+        <input class="odg_input" id="pitanje${num}_odg3_input" type='text' placeholder='Odgovor 3:'/> 
+        <input class="RadioButton" type="radio" name="pitanje${num}_radio" value="3">
+    </div>
+    `;
+
+    document.getElementById("Wrapper").innerHTML += questionTemplate; 
+}
+
+function generateTemplates(){
+    var entries = document.getElementById("formaPost");
+    var questionNum = entries.num_pitanja_input.value;
+
+    if(questionNum > 10){
+        entries.num_pitanja_input.value = 10;
+    }
+
+    if(questionNum < 0){
+        entries.num_pitanja_input.value = 0;
+    }
+
+    document.getElementById("Wrapper").innerHTML="";
+    for(let i = 1; i <= entries.num_pitanja_input.value; i++){
+        generateInput(i);
+    }
+}
+
+function validPosting(){
+    let validated = 1;
+    var entries = document.getElementById("formaPost");
+    var questionNum = entries.num_pitanja_input.value;
+
+    if(document.getElementById("naslov_input").value.length == 0){
+        validated = 0;
+    }
+
+    if(document.getElementById("sadrzaj_input").value.length == 0){
+        validated = 0;
+    }
+
+    for(let i = 1; i <= entries.num_pitanja_input.value; i++){
+        var pitanjeTekst = (document.getElementById("pitanje"+i+"_tekst_input").value);
+        var pitanje1 = (document.getElementById("pitanje"+i+"_odg1_input").value);
+        var pitanje2 = (document.getElementById("pitanje"+i+"_odg2_input").value);
+        var pitanje3 = (document.getElementById("pitanje"+i+"_odg3_input").value);
+        var tacanodg = document.querySelector('input[name='+ 'pitanje' + i + '_radio' +']:checked').value;
+
+        if(pitanjeTekst.length == 0 || pitanje1.length == 0 || pitanje2.length == 0 || pitanje3.length == 0 || tacanodg == null){
+            validated = 0;
+        }
+    }
+
+    if(validated == 1){
+        document.getElementById("PostError").classList.add("Hidden2");
+        objaviPost();
+    }else{
+        document.getElementById("PostError").classList.remove("Hidden2");
+    }
+}
+
+function objaviPost(){
+    var entries = document.getElementById("formaPost");
+    var questionNum = entries.num_pitanja_input.value;
+    var arr = [];
+
+    for(let i = 1; i <= entries.num_pitanja_input.value; i++){
+
+
+        var pitanjeTekst = (document.getElementById("pitanje"+i+"_tekst_input").value);
+        var pitanje1 = (document.getElementById("pitanje"+i+"_odg1_input").value);
+        var pitanje2 = (document.getElementById("pitanje"+i+"_odg2_input").value);
+        var pitanje3 = (document.getElementById("pitanje"+i+"_odg3_input").value);
+        var tacanodg = document.querySelector('input[name='+ 'pitanje' + i + '_radio' +']:checked').value;
+
+        var str = pitanjeTekst + '/.!./' + pitanje1 + '/.!./' + pitanje2 + '/.!./' + pitanje3 + '/.!./';
+        arr.push({
+            pitanje:str,
+            tacanOdgovor:tacanodg
+        });
+    }
+
+    console.log(arr);
+}
